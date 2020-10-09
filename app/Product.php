fd001835e741259b805cb0cbb753bcfb9f7af78c
Amin shojaei <shojaei.x@gmail.com>
2020-10-09 13:31:45 +0330

    Added category to product

--- app/Product.php
+++ app/Product.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ["name", "price", "expire_date"];
+    protected $fillable = ["name", "price", "expire_date", 'category_id'];
 
     protected $casts = [
         'expire_date' => 'datetime',
         'produced_at' => 'datetime'
     ];
 
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
